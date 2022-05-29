9120babd510b894060ebc3c24e3522e776bec58e
DmitryLancer20 <timofeev.dooma@yandex.ru>
2022-05-29 15:36:53 +0300

    добавил function()

--- controller/RegistrationController.php
+++ controller/RegistrationController.php
@@ -4,12 +4,21 @@ include __DIR__ . '/../view/index.html';
 
 function cleanParameters($value)
 {
-    var_dump($value);
+//    var_dump($value);
     $result = filter_var(trim($_POST[$value]), FILTER_SANITIZE_STRING);
-    var_dump($result);
+//    var_dump($result);
 
     return $result;
 }
+
+    $login = cleanParameters('login');
+    $name = cleanParameters('pass');
+    $pass = cleanParameters('name');
+    $age = cleanParameters('age');
+    $gender = cleanParameters('gender');
+
+
+
 function isLoginValid($login)
 {
     if (mb_strlen($login) < 5 || mb_strlen($login) > 90) {
@@ -19,25 +28,47 @@ function isLoginValid($login)
     }
 }
 
+function isPassValid($pass)
+{
+    if (mb_strlen($pass) < 2 || mb_strlen($pass) > 6) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
-$login = cleanParameters('login');
-$name = cleanParameters('name');
-$pass = cleanParameters('pass');
-$age = cleanParameters('age');
-$gender = cleanParameters('gender');
+function isNameValid($name)
+{
+    if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function isAgeValid($age)
+{
+    if ($age < 6 || $age > 70) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
 
 
 
 if (!isLoginValid($login)) {
-    echo 'Недопустимая длина логина';
+    echo 'Недопустимая длина логинаfffff';
 } else {
-    if (mb_strlen($pass) < 2 || mb_strlen($pass) > 6) {
+    if (!isPassValid($pass)) {
         echo 'Недопустимая длина пароля (от 2 до 6 символов)';
     } else {
-        if (mb_strlen($name) < 3 || mb_strlen($name) > 50) {
+        if (!isPassValid($name)) {
             echo 'Недопустимая длина имени';
         } else {
-            if ($age < 6 || $age > 70) {
+            if (!isPassValid($age)) {
                 echo 'Возраст (от 6 до 70 лет!)';
             } else {
                 if ($gender == '') {
@@ -48,7 +79,7 @@ if (!isLoginValid($login)) {
     }
 }
 
-// header('Location: /content-page.php', false, 301);
+//header('Location: /view/content-page.php');
 
 
 $mysql = new mysqli('localhost', 'root', 'root', 'test1');
--- view/content-page.php
+++ view/content-page.php
@@ -1,5 +1,5 @@
 <?php include_once 'content.php';
-require_once 'PHPExcel.php';
+
 ?>
 
 
