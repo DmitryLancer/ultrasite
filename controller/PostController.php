a8e33d34751148659253d63f9437ee5a7a9ec5d6
Alina <amanturova@secureducation.ru>
2022-08-07 16:52:48 +0300

    ошибки и рефортам

--- controller/PostController.php
+++ controller/PostController.php
@@ -2,9 +2,8 @@
 
 namespace controller;
 
-
 use model\DataBase;
-use PDO;
+use model\Post;
 
 class PostController extends Controller
 {
@@ -14,43 +13,34 @@ class PostController extends Controller
 
         require __DIR__ . '/../model/Post.php';
 
-        $post = new \model\Post();
+        $post = new Post();
 
         $database = new DataBase();
 
         $post->title = $this->cleanParameters('title');
         $post->body = $this->cleanParameters('body');
         $post->author_id = 1;
-//        echo '<pre>';
-//        var_dump($post);
-//        echo '<\pre>';
-//        die();
 
         $errors = [];
-
         $flag = 0;
 
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             if (!($_POST['action'] == 'registration')) {
-                if ($post->strTitle() < 2 ||  $post->strTitle() > 50) {
+                if ($post->strTitle() < 2 || $post->strTitle() > 50) {
                     $errors['title'] = '<small class="text-danger">Недопустимая длина заголовка (от 2 до 50 символов)</small>';
                     $flag = 1;
-
                 }
-                if ($post->strBody() < 10 ||  $post->strBody() > 250) {
+                if ($post->strBody() < 10 || $post->strBody() > 250) {
                     $errors['body'] = '<small class="text-danger">Недопустимая длина текста (от 10 до 250 символов)</small>';
                     $flag = 1;
-
                 }
-
                 if ($flag == 1) {
                     foreach ($errors as $error) {
                         echo $error;
                     }
                 }
-
                 if ($flag == 0) {
-                   // $database->savePost($post);
+                    // $database->savePost($post);
                     $sql = $post->prepareInsertSQL();
                     $parameters = $post->prepareParameters();
 
--- view/content.php
+++ view/content.php
@@ -14,14 +14,24 @@
 	<div class="container mt-4" style="width: 500px;">
 	<div class="alert alert-success">Аккаунт успешно зарегистрирован!</div>
 		<h1>Создание поста</h1>
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> <?php echo $errors['success'] ?>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <?php
+            if (!empty($errors['success'])) {
+                echo $errors['success'];
+            }?>
 			<p>Заголовок: </p>
 			<input type="text" class="form-control" name="title" id="title" value="<?php echo $_POST['title'] ?>"> <br>
-			<?php echo $errors['title'] ?>
+            <?php
+            if (!empty($errors['title'])) {
+                echo $errors['title'];
+            }?>
 			<p>Текст: </p>
 			<textarea style="width: 500px; height:500px" class="form-control" name="body" id="body" value="<?php echo $_POST['body'] ?>">
 			 </textarea><br>
-			 <?php echo $errors['body'] ?>
+            <?php
+            if (!empty($errors['body'])) {
+                echo $errors['body'];
+            }?>
 			<button style="margin: 0 auto; display: block; margin-top: 35px;" class="btn btn-success" type="submit">Отправить пост!</button>
             <input type="hidden" value="post" name="action">
 		</form>
